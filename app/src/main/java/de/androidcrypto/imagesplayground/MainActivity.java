c1d1e3d9ad91fba0fa362c93140a810df5afa741
FlutterCrypto <fluttercrypto@gmx.de>
2023-02-08 01:11:03 +0100

    updated

--- app/src/main/java/de/androidcrypto/imagesplayground/MainActivity.java
+++ app/src/main/java/de/androidcrypto/imagesplayground/MainActivity.java
@@ -1,25 +1,45 @@
 package de.androidcrypto.imagesplayground;
 
+import static android.os.Build.VERSION.SDK_INT;
+
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.ImageDecoder;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.util.Size;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,6 +57,11 @@ public class MainActivity extends AppCompatActivity {
     ImageView imageConversion;
     TextView imageConversionSizes;
 
+    Button saveBitmapToExternalSharedStorage;
+    TextView saveBitmapToExternalSharedStorageLogFile;
+
+    private Bitmap bitmap;
+    private ActivityResultLauncher<String> storageResultActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +80,11 @@ public class MainActivity extends AppCompatActivity {
         imageConversion = findViewById(R.id.ivBitmapConversion);
         imageConversionSizes = findViewById(R.id.tvBitmapConversionSizes);
 
+        saveBitmapToExternalSharedStorage = findViewById(R.id.btnSaveBitmapToExternalSharedStorage);
+        saveBitmapToExternalSharedStorageLogFile = findViewById(R.id.tvSaveBitmapToExternalSharedStorage);
+
+        registerWriteExternalStoragePermission();
+
         loadImageFromGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -69,6 +99,8 @@ public class MainActivity extends AppCompatActivity {
                 Log.i(TAG, "scale down image");
                 BitmapDrawable draw = (BitmapDrawable) imageOriginal.getDrawable();
                 Bitmap original = draw.getBitmap();
+
+                bitmap = original;
 
                 Bitmap resized = scaleDown(original, 300, true);
                 imageScaledDown.setImageBitmap(resized);
@@ -89,13 +121,182 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Log.i(TAG, "bitmap to bytearray conversion");
 
-                /*
-                Bitmap bmp = BitmapFactory.decodeByteArray(decryptedPicture, 0, decryptedPicture.length);
-                ImageView image = (ImageView) findViewById(R.id.imageView2);
-                image.setImageBitmap(Bitmap.createScaledBitmap(bmp, bmp.getWidth(), bmp.getHeight(), false));
-                */
+                Bitmap bitmap = ((BitmapDrawable) imageOriginal.getDrawable()).getBitmap();
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG,10,stream);
+                byte[] byteArray = stream.toByteArray();
+                Bitmap compressedBitmap = BitmapFactory.decodeByteArray(byteArray,0,byteArray.length);
+                imageConversion.setImageBitmap(compressedBitmap);
+                String sizesData = "imageByteArray length: " + byteArray.length + "\n";
+                sizesData += "Image sizes width: " + compressedBitmap.getWidth() +
+                        " height: " + compressedBitmap.getHeight() +
+                        " pixel: " + (compressedBitmap.getWidth() * compressedBitmap.getHeight());
+                imageConversionSizes.setText(sizesData);
+/*
+                byte[] imageByteArray = getBytes(imageOriginal);
+                String sizesData = "imageByteArray length: " + imageByteArray.length + "\n";
+
+                Bitmap bmp = BitmapFactory.decodeByteArray(imageByteArray, 0, imageByteArray.length);
+                //ImageView image = (ImageView) findViewById(R.id.imageView2);
+                imageConversion.setImageBitmap(Bitmap.createScaledBitmap(bmp, bmp.getWidth(), bmp.getHeight(), false));
+                sizesData += "Image sizes width: " + bmp.getWidth() +
+                        " height: " + bmp.getHeight() +
+                        " pixel: " + (bmp.getWidth() * bmp.getHeight());
+                imageConversionSizes.setText(sizesData);
+*/
             }
         });
+
+        saveBitmapToExternalSharedStorage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "save bitmap to external shared storage");
+
+                // https://www.youtube.com/watch?v=nA4XWsG9IPM
+                // How to Save Image to External Storage using Java API 30+ || Scoped Storage android Q R || Java
+
+                //registerWriteExternalStoragePermission();
+                //storageResultActivity.launch("");
+            }
+        });
+    }
+
+    /**
+     * section for storage of image files
+     * source https://stackoverflow.com/a/70766323/8166854
+     * author: answered Jan 19, 2022 at 6:47 by OneDev
+     */
+
+    private void registerWriteExternalStoragePermission() {
+        storageResultActivity = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+            if (isGranted) {
+                checkStoragePermissionAndSaveImage(bitmap);
+            }
+        });
+    }
+
+    private void checkStoragePermissionAndSaveImage(Bitmap bitmap) {
+        Log.i(TAG, "checkStoragePermissionAndSaveImage");
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "now: saveImageInAndroidApi28AndBelow(bitmap)");
+            saveImageInAndroidApi28AndBelow(bitmap);
+        } else if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            //show user app can't save image for storage permission denied
+        } else {
+            Log.i(TAG, "storageResultActivity.launch");
+            storageResultActivity.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+
+    }
+
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+        if (result.getResultCode() == RESULT_OK) {
+            Intent data = result.getData();
+            if (data == null || data.getData() == null) {
+                //showError
+            }
+            Uri uri = data.getData();
+            if (Build.VERSION.SDK_INT < 29) {
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                ImageDecoder.Source source = ImageDecoder.createSource(getContentResolver(), uri);
+                try {
+                    bitmap = ImageDecoder.decodeBitmap(source);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    });
+
+
+        /*
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            saveImageInAndroidApi28AndBelow(bitmap);
+        } else if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            //show user app can't save image for storage permission denied
+        } else {
+            storageResultActivity.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+
+         */
+
+    private boolean saveImageInAndroidApi28AndBelow(Bitmap bitmap) {
+        OutputStream fos;
+        String imagesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).toString();
+        File image = new File(imagesDir, "IMG_" + System.currentTimeMillis() + ".png");
+        try {
+            fos = new FileOutputStream(image);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 95, fos);
+            Objects.requireNonNull(fos).close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            //isSuccess = false;
+            return false;
+        }
+        //isSuccess = true;
+        return true;
+    }
+
+    public Uri saveImageInAndroidApi29AndAbove(@NonNull final Bitmap bitmap) throws IOException {
+        final ContentValues values = new ContentValues();
+        values.put(MediaStore.MediaColumns.DISPLAY_NAME, "IMG_" + System.currentTimeMillis());
+        values.put(MediaStore.MediaColumns.MIME_TYPE, "image/png");
+        if (SDK_INT >= Build.VERSION_CODES.Q) {
+            values.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DCIM);
+        }
+        final ContentResolver resolver = getContentResolver();
+        Uri uri = null;
+        try {
+            final Uri contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+            uri = resolver.insert(contentUri, values);
+            if (uri == null) {
+                //isSuccess = false;
+                throw new IOException("Failed to create new MediaStore record.");
+            }
+            try (final OutputStream stream = resolver.openOutputStream(uri)) {
+                if (stream == null) {
+                    //isSuccess = false;
+                    throw new IOException("Failed to open output stream.");
+                }
+                if (!bitmap.compress(Bitmap.CompressFormat.PNG, 95, stream)) {
+                    //isSuccess = false;
+                    throw new IOException("Failed to save bitmap.");
+                }
+            }
+            //isSuccess = true;
+            return uri;
+        } catch (IOException e) {
+            if (uri != null) {
+                resolver.delete(uri, null, null);
+            }
+            throw e;
+        }
+    }
+
+
+    /**
+     * section for storage of image files END
+     */
+
+    public byte[] getBytes(ImageView imageView) {
+        try {
+            Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            //bitmap.compress(Bitmap.CompressFormat.JPEG, 40, stream);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            byte[] bytesData = stream.toByteArray();
+            stream.close();
+            return bytesData;
+        } catch(Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        //return null;
     }
 
     public static Bitmap scaleDown(Bitmap realImage, float maxImageSize,
